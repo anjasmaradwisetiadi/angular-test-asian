4a142f914ef9d0d41565d031ad6064123d99376c
diamondto <enzoblackred@gmail.com>
2024-06-08 13:38:37 +0700

    [feat/employee] successfull make dashboard profile

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dataUserInterface } from 'src/app/interface/authInterface';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  profile:any;
   constructor() { }
 
   ngOnInit(): void {
+    let getProfileUser = localStorage.getItem('user');
+    getProfileUser = getProfileUser ? JSON.parse(getProfileUser) : '' 
+    this.profile = getProfileUser;
   }
 
 }
