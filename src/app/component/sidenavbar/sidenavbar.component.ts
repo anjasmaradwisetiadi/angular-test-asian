8e23e0b2d748ad01bc67cea3a9fca8492151605e
diamondto <enzoblackred@gmail.com>
2024-06-09 00:56:45 +0700

    [feat/employee] fixing navigate link

--- src/app/component/sidenavbar/sidenavbar.component.ts
+++ src/app/component/sidenavbar/sidenavbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-sidenavbar',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavbarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( private route:ActivatedRoute, private location: Location ) { }
+  routeActive:string = '';
   ngOnInit(): void {
+    this.routeActive = this.location.path();
   }
 
 }
